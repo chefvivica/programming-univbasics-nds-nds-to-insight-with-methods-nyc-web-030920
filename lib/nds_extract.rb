b6ffdbfb81efc5a28a6539628dc6e2846d60579b
Vivica Ma <vivicacongma@gmail.com>
2020-02-21 04:27:27 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -22,7 +22,7 @@ def directors_totals(nds)
   result = {}
   index = 0 
     while index < nds.length do 
-      result = nds[index][:names]=>gross_for_director(nds)
+      result = nds[index][:names] += gross_for_director(nds)
     index += 1 
   end 
 end
